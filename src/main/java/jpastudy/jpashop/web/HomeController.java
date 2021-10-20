50c6dc264352fbed17af4278c1422c4ef2ddcc85
hoseokjang <hoseok210@gmail.com>
2021-10-20 16:15:53 +0900

    RestAPI/Thymeleaf - HomeController 2

--- src/main/java/jpastudy/jpashop/web/HomeController.java
+++ src/main/java/jpastudy/jpashop/web/HomeController.java
@@ -7,10 +7,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @Slf4j
 public class HomeController {
-    @RequestMapping("/")
+    @RequestMapping("/frag")
     public String home()
     {
+        // header 연습용
         log.info("home controller");
         return "home";
+    }
+    @RequestMapping("/") // getmapping 해도 됨
+    public String index()
+    {   // 진짜 Home
+        return "index";
     }
 }
