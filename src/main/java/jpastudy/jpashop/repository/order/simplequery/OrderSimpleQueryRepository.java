b6867d89bc136fd706940e1feaaad73cc96b7416
hoseokjang <hoseok210@gmail.com>
2021-10-21 17:03:07 +0900

    Order Simple Query v4

--- src/main/java/jpastudy/jpashop/repository/order/simplequery/OrderSimpleQueryRepository.java
+++ src/main/java/jpastudy/jpashop/repository/order/simplequery/OrderSimpleQueryRepository.java
@@ -12,7 +12,7 @@ public class OrderSimpleQueryRepository {
     private final EntityManager em;
     public List<OrderSimpleQueryDto> findOrderDtos() {
         return em.createQuery(
-                        "select new jpastudy.jpashop.repository.order.simplequery.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
+                        "select new jpastudy.jpashop.repository.order.simplequery.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.orderStatus, d.address)" +
                         " from Order o" +
                                 " join o.member m" +
                                 " join o.delivery d", OrderSimpleQueryDto.class)
