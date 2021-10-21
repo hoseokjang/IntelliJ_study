5eee666448e6e151f8809fba101863e0a88d1e0d
hoseokjang <hoseok210@gmail.com>
2021-10-21 17:44:36 +0900

    Order Api (*ToMany) v2

--- src/main/java/jpastudy/jpashop/api/OrderApiController.java
+++ src/main/java/jpastudy/jpashop/api/OrderApiController.java
@@ -1,14 +1,16 @@
 package jpastudy.jpashop.api;
 
-import jpastudy.jpashop.domain.Order;
-import jpastudy.jpashop.domain.OrderItem;
-import jpastudy.jpashop.domain.OrderSearch;
+import jpastudy.jpashop.domain.*;
 import jpastudy.jpashop.repository.OrderRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 /*
   One-To-Many 관계 성능 최적화
@@ -41,4 +43,44 @@ public class OrderApiController {
         v2 : Entity를 Dto로 변환해서 노출하는 방식
         N+1 문제는 여전히 존재
      */
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2()
+    {
+        List<Order> orders = orderRepository.findAll(new OrderSearch());
+        return orders.stream()
+                .map(order -> new OrderDto(order))
+                .collect(toList());
+    }
+    // -- 응답과 요청에 사용할 DTO Inner Class 선언 -- //
+    @Data
+    static class OrderItemDto {
+        private String itemName; //상품 명
+        private int orderPrice; //주문 가격
+        private int count; //주문 수량
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
+    @Data
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getOrderStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream() // Stream<OrderItem>
+                    .map(orderItem -> new OrderItemDto(orderItem)) // Stream<OrderItemDto>
+                    .collect(toList()); // List<OrderItemDto>
+        }
+    }
+
 }
