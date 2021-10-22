83d71d86da32cf6ac753df13c37a79566e46ced9
hoseokjang <hoseok210@gmail.com>
2021-10-22 11:41:43 +0900

    Groupby Test version.2

--- src/test/java/jpastudy/jpashop/GroupbyTest.java
+++ src/test/java/jpastudy/jpashop/GroupbyTest.java
@@ -3,7 +3,9 @@ package jpastudy.jpashop;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
@@ -25,6 +27,12 @@ public class GroupbyTest {
         // Dish의 이름을 구분자를 포함한 문자열로 출력
         String namestr = dishList.stream().map(Dish::getName).collect(joining(","));
         System.out.println(namestr);
+        System.out.println("==============================");
+        // Dish의 칼로리 합계, 평균 등등 계산
+        Integer totalCalory = dishList.stream().collect(Collectors.summingInt(Dish::getCalory));
+        System.out.println("합계 : " + totalCalory);
+        IntSummaryStatistics statistics = dishList.stream().collect(Collectors.summarizingInt(Dish::getCalory));
+        System.out.println("통계 : " + statistics);
     }
 
     static class Dish
